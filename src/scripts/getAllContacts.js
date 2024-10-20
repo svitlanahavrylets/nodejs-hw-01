e8daad34b53cbe2f3c949a648d2f0f79eeff0c5a
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-10-20 22:22:40 +0200

    add all

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,7 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  return readContacts();
+};
 
 console.log(await getAllContacts());
